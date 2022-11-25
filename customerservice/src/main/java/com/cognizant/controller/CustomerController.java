5ac04bb8912e80678e17e7d5c18338c9b197bde0
Adagbasa <2050610@cognizant.com>
2022-11-25 16:37:00 +0100

    adding exceptions for the given steps

--- customerservice/src/main/java/com/cognizant/controller/CustomerController.java
+++ customerservice/src/main/java/com/cognizant/controller/CustomerController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cognizant.dto.CustomerDto;
+import com.cognizant.exceptions.CreateCustomerFailedException;
+import com.cognizant.exceptions.CustomerNotFoundException;
 import com.cognizant.model.Customer;
 import com.cognizant.model.CustomerCreationStatus;
 import com.cognizant.service.CustomerService;
@@ -47,37 +49,49 @@ public class CustomerController {
 	@PostMapping("/createCustomer")
 	public CustomerCreationStatus createCustomer(
 			@Parameter(description = "Customer object to be saved") @Valid @RequestBody CustomerDto customer,
-			BindingResult bindingResult) throws DateTimeException, BindException {
+			BindingResult bindingResult) throws DateTimeException, BindException, CreateCustomerFailedException {
 		log.info("Inside the createCustomer EndPoint : Begin");
 
 		if (bindingResult.hasErrors()) {
 			throw new BindException();
 		}
 		Customer newCustomer = customer.getCustomerFromDto();
-		customerService.createCustomer(newCustomer);
+		Customer createdCustomer = customerService.createCustomer(newCustomer);
+
+		if (createdCustomer == null) {
+			throw new CreateCustomerFailedException("Customer Creation is UNSUCCESSFUL");
+		}
 
 		log.info("Inside the createCustomer EndPoint : End");
-		return new CustomerCreationStatus("Customer Creation SUCCESSFUL", newCustomer.getCustomerid());
+		return new CustomerCreationStatus("Customer Creation SUCCESSFUL", createdCustomer.getCustomerid());
 	}
 
 	@Operation(summary = "Used to retrieve customer details including a list of customer accounts.")
 	@GetMapping("/getCustomerDetails/{customerId}")
 	public Customer getCustomerDetails(
-			@Parameter(description = "id of customer to be searched") @PathVariable(value = "customerId") String customerId) {
+			@Parameter(description = "id of customer to be searched") @PathVariable(value = "customerId") String customerId)
+			throws CustomerNotFoundException {
 		log.info("Inside the getCustomerDetails EndPoint : Begin");
 		Customer customerWithDetails = customerService.getCustomerDetails(customerId);
+		if (customerWithDetails == null) {
+			throw new CustomerNotFoundException("Customer Details Not Found");
+		}
 		log.info("Inside the getCustomerDetails EndPoint : End");
 		return customerWithDetails;
 	}
 
 	@PostMapping("/updateCustomer")
 	public Customer updateCustomer(@Valid @RequestBody CustomerDto customer) {
+		log.info("Inside the updateCustomer EndPoint : Start");
 		Customer newCustomer = customer.getCustomerFromDto();
+		log.info("Inside the updateCustomer EndPoint : End");
 		return customerService.updateCustomer(newCustomer);
 	}
 
 	@DeleteMapping("deleteCustomer/{customerId}")
 	public Boolean deleteCustomer(@PathVariable(value = "customerId") String customerId) {
+		log.info("Inside the deleteCustomer EndPoint : Start");
+		log.info("Inside the deleteCustomer EndPoint : End");
 		return customerService.deleteCustomer(customerId);
 	}
 
